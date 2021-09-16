ce46dc54e4c0dfab3e4c2be42d37ca83ea311b19
Vladislav <mega.nozhkin@mail.ru>
2021-09-16 20:06:12 +0300

    init webpack-base

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+
+  mode: "development",
+
+  entry: {
+    main: './src/index.js'
+  },
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('./build')
+  },
+
+  module: {},
+
+  plugins: {},
+
+
+}
