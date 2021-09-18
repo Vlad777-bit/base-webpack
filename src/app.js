36e88bfc4b1aded656e3692dafc96e6f54c63018
Vladislav <mega.nozhkin@mail.ru>
2021-09-18 13:30:13 +0300

    Базовая настройка webpack

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import "./style/css.scss";
+import app from './components/main';
+
+app();
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,53 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-
-  mode: "development",
+  mode: 'development',
 
   entry: {
-    main: './src/index.js'
+    main: './src/app.js',
   },
 
   output: {
-    filename: 'bundle.js',
-    path: path.resolve('./build')
+    filename: './script/bundle.js',
+    path: path.resolve('./dist'),
   },
 
-  module: {},
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
 
-  plugins: {},
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
 
+    new MiniCssExtractPlugin({
+      filename: 'style/[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
 
-}
+  devServer: {
+    port: 3000,
+    open: false,
+  },
+};
